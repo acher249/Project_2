05336d6d624415f5942f3e2b3f33a909ef267725
Adam Chernick <jchernick2010@gmail.com>
2018-10-04 19:26:30 -0400

    clean working endpoints

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
